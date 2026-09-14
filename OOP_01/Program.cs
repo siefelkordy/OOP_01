d9cc6b75d9f3e0a945b52435d36151ec732fa286
siefelkordy <seifemadelkordy@gmail.com>
2026-09-15 00:31:08 +0300

    2nd requirement

--- OOP_01/Program.cs
+++ OOP_01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace OOP_01
+﻿using System.Globalization;
+
+namespace OOP_01
 {
     internal class Program
     {
@@ -20,9 +22,102 @@
             }
 
         }
+        internal struct Shipment
+        {
+            int TrackingCode;
+            string Description;
+            int Weight;
+            decimal DeliveryFee;
+
+            public string GetDestination(DeliveryAddress adress)
+            {
+                return (adress.GetFullAddress());
+
+            }
+            public void SetTrackingCode(int trackingCode)
+            {
+                if (trackingCode > 0)
+                {
+                    TrackingCode = trackingCode;
+                }
+                else if (trackingCode < 0)
+                {
+                    throw new ArgumentException("Tracking code must be a positive number.");
+
+                }
+                else if (trackingCode.ToString() is null || trackingCode.ToString() == " " || trackingCode.ToString() == string.Empty)
+                {
+                    throw new ArgumentException("Tracking code cannot be null or empty.");
+
+                }
+                this.TrackingCode = trackingCode;
+            }
+            public void SetDescription(string description)
+            {
+                if (string.IsNullOrWhiteSpace(description) || description == string.Empty)
+                {
+                    throw new ArgumentException("Description cannot be null or empty.");
+                }
+                this.Description = description;
+            }
+
+            public void SetWeight(int weight)
+            {
+                if (weight <= 0)
+                {
+                    throw new ArgumentException("Weight must be a positive number.");
+                }
+                this.Weight = weight;
+            }
+            public void SetDeliveryFee(decimal fee)
+            {
+                if (fee <= 0)
+                {
+                    throw new ArgumentException("Delivery fee must be a positive number.");
+                }
+                this.DeliveryFee = fee;
+            }
+            public decimal GetDeliveryFee()
+            {
+                return DeliveryFee + (Weight * 5);
+            }
+            //Constructor
+            public Shipment(string TrackingCode)
+            {
+                SetTrackingCode(int.Parse(TrackingCode));
+                this.Description = "Unknown";
+                Weight = 1;
+                DeliveryFee = 50;
+                GetDestination(new DeliveryAddress("New York", "5th Avenue", 123));
+            }
+            public Shipment(string TrackingCode, string Description, int Weight, decimal DeliveryFee)
+            {
+                SetTrackingCode(int.Parse(TrackingCode));
+                SetDescription(Description);
+                SetWeight(Weight);
+                SetDeliveryFee(DeliveryFee);
+                GetDestination(new DeliveryAddress("New York", "5th Avenue", 123));
+            }
+            public void UpdateDeilveryFee(decimal newFee)
+            {
+                if(newFee>0)
+                {
+                    DeliveryFee = newFee;
+                }
+                else
+                {
+                    throw new ArgumentException("Delivery fee must be a positive number.");
+                }
+            }
+            public string PrintShipmentDetails()
+            {
+                return $"Tracking Code: {TrackingCode}, Description: {Description}, Weight: {Weight}kg, Delivery Fee: ${GetDeliveryFee()}";
+            }
+
+        }
 
 
-        static void Main(string[] args)
+                static void Main(string[] args)
         {
             DeliveryAddress address = new DeliveryAddress("New York", "5th Avenue", 123);
             DeliveryAddress address1 = address;
