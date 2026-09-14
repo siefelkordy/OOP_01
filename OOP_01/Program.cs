e4338474edbb51c08d500cf1eaa08120bc5e3bb8
siefelkordy <seifemadelkordy@gmail.com>
2026-09-14 23:30:20 +0300

    Add project files.

---
+++ OOP_01/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OOP_01
+{
+    internal class Program
+    {
+        internal struct DeliveryAddress
+        {
+            public string City;
+            public string Street;
+            public int BuildingNumber;
+
+            public DeliveryAddress(string city, string street, int buildingNumber)
+            {
+                City = city;
+                Street = street;
+                BuildingNumber = buildingNumber;
+            }
+            public string GetFullAddress()
+            {
+                return $"{BuildingNumber} {Street} ,{City}";
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            DeliveryAddress address = new DeliveryAddress("New York", "5th Avenue", 123);
+            DeliveryAddress address1 = address;
+            address1.City = "Los Angeles";
+            address1.Street = "Sunset Boulevard";
+            address1.BuildingNumber = 456;
+            Console.WriteLine(address.GetFullAddress());
+            Console.WriteLine(address1.GetFullAddress());
+        }
+    }
+}
